20120a22366b0443a618073146f4b036089eb8e7
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2016-08-15 16:20:36 -0300

    added acceptance boilerplate

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,20 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'emberjs-tutorial/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list rentals');
+
+test('should list available rentals', (assert) => {
+
+})
+
+test('should link the information about the company', (assert) => {
+
+})
+
+test('should link to contact information', (assert) => {
+
+})
+
+test('should filter the list of rentals by city', (assert) => {
+
+})
